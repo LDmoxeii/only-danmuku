e2d297cfdcb9334dea37008ca7f22f2caf70c54f
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-09-27 17:09:08 +0800

    feat(sql): 更新数据库字段默认值和枚举类型

--- design/aggregate/customer_profile/customer_profile.sql
+++ design/aggregate/customer_profile/customer_profile.sql
@@ -4,18 +4,18 @@
 DROP TABLE IF EXISTS `customer_profile`;
 CREATE TABLE `customer_profile`
     (
-    `id`                  bigint                                                       NOT NULL COMMENT 'ID',
+    `id`                  bigint                                                        NOT NULL COMMENT 'ID',
     `nick_name`           varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '昵称',
     `avatar`              varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT NULL COMMENT '头像',
     `email`               varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '邮箱',
-    `sex`                 tinyint(1)                                                    NULL     DEFAULT NULL COMMENT '性别 @E=0:FEMALE:女|1:MALE:男|2:UNKNOWN:未知;@T=SexType',
+    `sex`                 tinyint(1)                                                    NOT NULL DEFAULT 0 COMMENT '性别 @E=0:UNKNOWN:未知|1:FEMALE:女|2:MALE:男|;@T=SexType',
     `birthday`            varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NULL     DEFAULT NULL COMMENT '出生日期',
     `school`              varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT NULL COMMENT '学校',
     `person_introduction` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT NULL COMMENT '个人简介',
     `notice_info`         varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT NULL COMMENT '空间公告',
     `total_coin_count`    int(11)                                                       NOT NULL COMMENT '硬币总数量',
     `current_coin_count`  int(11)                                                       NOT NULL COMMENT '当前硬币数',
-    `theme`               tinyint(1)                                                    NOT NULL DEFAULT 1 COMMENT '主题 @E=1:LIGHT:浅色主题|2:DARK:深色主题|3:SYSTEM:跟随系统;@T=ThemeType',
+    `theme`               tinyint(1)                                                    NOT NULL DEFAULT 0 COMMENT '主题 @E=0:UNKNOW:未知主题|1:LIGHT:浅色主题|2:DARK:深色主题|3:SYSTEM:跟随系统;@T=ThemeType',
     `create_user_id`      bigint                                                        NULL     DEFAULT NULL COMMENT '创建人ID',
     `create_by`           varchar(32)                                                   NULL     DEFAULT NULL COMMENT '创建人名称',
     `create_time`         bigint                                                        NULL     DEFAULT NULL COMMENT '创建时间',
--- design/aggregate/statistics/statistics.sql
+++ design/aggregate/statistics/statistics.sql
@@ -6,16 +6,16 @@ CREATE TABLE `statistics_info`
     (
     `id`               bigint      NOT NULL COMMENT 'ID',
     `customer_id`      bigint      NOT NULL COMMENT '用户ID',
-    `data_type`        tinyint(1)  NOT NULL COMMENT '数据统计类型 @E=1:VIDEO_VIEW:视频观看|2:VIDEO_LIKE:视频点赞|3:VIDEO_COMMENT:视频评论|4:VIDEO_SHARE:视频分享|5:USER_FOLLOW:用户关注|6:USER_LOGIN:用户登录;@T=StatisticsDataType',
-    `statistics_count` int(11)     NULL DEFAULT NULL COMMENT '统计数量',
+    `data_type`        tinyint(1)  NOT NULL DEFAULT 0 COMMENT '数据统计类型 @E=0:UNKNOW:未知类型|1:VIDEO_VIEW:视频观看|2:VIDEO_LIKE:视频点赞|3:VIDEO_COMMENT:视频评论|4:VIDEO_SHARE:视频分享|5:USER_FOLLOW:用户关注|6:USER_LOGIN:用户登录;@T=StatisticsDataType',
+    `statistics_count` int(11)     NULL     DEFAULT NULL COMMENT '统计数量',
     `statistics_date`  bigint      NOT NULL COMMENT '统计日期',
-    `create_user_id`   bigint      NULL DEFAULT NULL COMMENT '创建人ID',
-    `create_by`        varchar(32) NULL DEFAULT NULL COMMENT '创建人名称',
-    `create_time`      bigint      NULL DEFAULT NULL COMMENT '创建时间',
-    `update_user_id`   bigint      NULL DEFAULT NULL COMMENT '更新人ID',
-    `update_by`        varchar(32) NULL DEFAULT NULL COMMENT '更新人名称',
-    `update_time`      bigint      NULL DEFAULT NULL COMMENT '更新时间',
-    `deleted`          tinyint(1)       DEFAULT 0 NOT NULL COMMENT '删除标识 0：未删除 id：已删除',
+    `create_user_id`   bigint      NULL     DEFAULT NULL COMMENT '创建人ID',
+    `create_by`        varchar(32) NULL     DEFAULT NULL COMMENT '创建人名称',
+    `create_time`      bigint      NULL     DEFAULT NULL COMMENT '创建时间',
+    `update_user_id`   bigint      NULL     DEFAULT NULL COMMENT '更新人ID',
+    `update_by`        varchar(32) NULL     DEFAULT NULL COMMENT '更新人名称',
+    `update_time`      bigint      NULL     DEFAULT NULL COMMENT '更新时间',
+    `deleted`          tinyint(1)           DEFAULT 0 NOT NULL COMMENT '删除标识 0：未删除 id：已删除',
     PRIMARY KEY (`id`) USING BTREE
     )
     ENGINE = InnoDB
