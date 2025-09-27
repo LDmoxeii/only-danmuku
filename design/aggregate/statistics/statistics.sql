4d2407f88d7f9b64dc8e57b10d7d55e71b50e4cc
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-09-27 16:57:45 +0800

    chore(sql): 更新数据库枚举注释格式

--- design/aggregate/statistics/statistics.sql
+++ design/aggregate/statistics/statistics.sql
@@ -6,7 +6,7 @@ CREATE TABLE `statistics_info`
     (
     `id`               bigint      NOT NULL COMMENT 'ID',
     `customer_id`      bigint      NOT NULL COMMENT '用户ID',
-    `data_type`        tinyint(1)  NOT NULL COMMENT '数据统计类型 @E=1:VIDEO_VIEW:视频观看|2:VIDEO_LIKE:视频点赞|3:VIDEO_COMMENT:视频评论|4:VIDEO_SHARE:视频分享|5:USER_FOLLOW:用户关注|6:USER_LOGIN:用户登录@T=StatisticsDataType',
+    `data_type`        tinyint(1)  NOT NULL COMMENT '数据统计类型 @E=1:VIDEO_VIEW:视频观看|2:VIDEO_LIKE:视频点赞|3:VIDEO_COMMENT:视频评论|4:VIDEO_SHARE:视频分享|5:USER_FOLLOW:用户关注|6:USER_LOGIN:用户登录;@T=StatisticsDataType',
     `statistics_count` int(11)     NULL DEFAULT NULL COMMENT '统计数量',
     `statistics_date`  bigint      NOT NULL COMMENT '统计日期',
     `create_user_id`   bigint      NULL DEFAULT NULL COMMENT '创建人ID',
