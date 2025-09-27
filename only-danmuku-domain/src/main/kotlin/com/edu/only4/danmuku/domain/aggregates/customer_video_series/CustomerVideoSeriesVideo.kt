4af9105d17d871d09820dba48f150742ddeb5a19
LD_moxeii <15976781430@163.com>
2025-09-27 22:24:28 +0800

    fix: update JoinColumn attributes to prevent insert and update operations on customerVideoSeries

--- only-danmuku-domain/src/main/kotlin/com/edu/only4/danmuku/domain/aggregates/customer_video_series/CustomerVideoSeriesVideo.kt
+++ only-danmuku-domain/src/main/kotlin/com/edu/only4/danmuku/domain/aggregates/customer_video_series/CustomerVideoSeriesVideo.kt
@@ -1,14 +1,9 @@
 package com.edu.only4.danmuku.domain.aggregates.customer_video_series
 
 import com.only4.cap4k.ddd.core.domain.aggregate.annotation.Aggregate
-
 import jakarta.persistence.*
-import org.hibernate.annotations.DynamicInsert
-import org.hibernate.annotations.DynamicUpdate
-import org.hibernate.annotations.Fetch
-import org.hibernate.annotations.GenericGenerator
-import org.hibernate.annotations.SQLDelete
-import org.hibernate.annotations.Where
+import jakarta.persistence.Table
+import org.hibernate.annotations.*
 
 /**
  * 用户视频序列视频关联;
@@ -68,7 +63,7 @@ class CustomerVideoSeriesVideo (
 ) {
 
     @ManyToOne(cascade = [], fetch = FetchType.EAGER)
-    @JoinColumn(name = "`series_id`", nullable = false)
+    @JoinColumn(name = "`series_id`", nullable = false, insertable = false, updatable = false)
     var customerVideoSeries: com.edu.only4.danmuku.domain.aggregates.customer_video_series.CustomerVideoSeries? = null
 
     // 【字段映射结束】本段落由[cap4k-ddd-codegen-gradle-plugin]维护，请不要手工改动
