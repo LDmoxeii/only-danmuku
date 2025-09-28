8ab5a5a1fc3c61c0595bea3d3a9c2c1fcb5f5223
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-09-28 09:56:41 +0800

    chore(deps): 更新 cap4k 依赖版本至 0.3.9-SNAPSHOT

--- template/_tpl/kotlin/Specification.kt
+++ template/_tpl/kotlin/Specification.kt
@@ -6,12 +6,6 @@ import com.only4.cap4k.ddd.core.domain.aggregate.Specification.Result
 import com.only4.cap4k.ddd.core.domain.aggregate.annotation.Aggregate
 import org.springframework.stereotype.Service
 
-/**
- * ${CommentEscaped}
- *
- * @author cap4k-ddd-codegen
- * @date ${date}
- */
 @Service
 @Aggregate(aggregate = "${Entity}", name = "${Entity}Specification", type = Aggregate.TYPE_SPECIFICATION, description = "")
 class ${Entity}Specification : Specification<${Entity}> {
