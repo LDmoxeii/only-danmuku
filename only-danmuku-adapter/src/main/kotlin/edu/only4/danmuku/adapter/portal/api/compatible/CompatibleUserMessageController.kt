0223af39ee87b225b8953be9a837218d82b791df
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-10-27 18:11:42 +0800

    refactor(api):重构消息控制器以支持兼容性

--- only-danmuku-adapter/src/main/kotlin/edu/only4/danmuku/adapter/portal/api/compatible/CompatibleUserMessageController.kt
+++ only-danmuku-adapter/src/main/kotlin/edu/only4/danmuku/adapter/portal/api/compatible/CompatibleUserMessageController.kt
@@ -1,16 +1,24 @@
-package edu.only4.danmuku.adapter.portal.api
+package edu.only4.danmuku.adapter.portal.api.compatible
 
-import edu.only4.danmuku.adapter.portal.api.payload.*
+import edu.only4.danmuku.adapter.portal.api.payload.MessageDel
+import edu.only4.danmuku.adapter.portal.api.payload.MessageGetNoReadCount
+import edu.only4.danmuku.adapter.portal.api.payload.MessageGetNoReadCountGroup
+import edu.only4.danmuku.adapter.portal.api.payload.MessageLoad
+import edu.only4.danmuku.adapter.portal.api.payload.MessageReadAll
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * 用户消息控制器
  */
 @RestController
-@RequestMapping("/message/v2")
+@RequestMapping("/message")
 @Validated
-class MessageController {
+class CompatibleUserMessageController {
 
     /**
      * 获取未读消息数
