6d9bd989e67e4f6defc8d06d57eec60a0fc56b75
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-20 15:28:49 +0800

    feat(account): 更新账户安全相关接口定义
    - 将生成短信验证码接口标记从 cli 改为 payload
    - 修改接口名称 GenerateSmsCode 为 LoginBySms
    - 调整接口描述为使用短信验证码登录
    - 删除旧的 GenerateSmsCodeCliHandler 处理器文件
    - 新增 LoginBySms 的请求和响应对象定义
    - 移除原有的 GenerateSmsCodeCli 接口参数定义文件

---
+++ only-danmuku-adapter/src/main/kotlin/edu/only4/danmuku/adapter/portal/api/payload/LoginBySms.kt
@@ -0,0 +1,26 @@
+package edu.only4.danmuku.adapter.portal.api.payload
+
+import org.mapstruct.Mapper
+import org.mapstruct.factory.Mappers
+
+/**
+ * 使用短信验证码登录
+ *
+ * 该文件由 [cap4k-ddd-codegen-gradle-plugin] 生成
+ * @author cap4k-ddd-codegen
+ * @date 2025/11/20
+ */
+object LoginBySms {
+
+    class Request
+
+    class Response
+
+    @Mapper(componentModel = "default")
+    interface Converter {
+        companion object {
+            val INSTANCE: Converter = Mappers.getMapper(Converter::class.java)
+        }
+    }
+}
+
