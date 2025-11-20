6bf06b77ac99aa8a46a2d936b577db8332e4d274
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-20 14:15:29 +0800

    refactor(user): 移除不必要的注释和枚举导入
    - 删除了 Jimmer DTO 查询相关的冗余注释
    - 移除了未使用的 UserType 枚举导入
    - 简化了查询逻辑，提高代码可读性

--- only-danmuku-adapter/src/main/kotlin/edu/only4/danmuku/adapter/application/queries/user/GetAccountInfoByEmailQryHandler.kt
+++ only-danmuku-adapter/src/main/kotlin/edu/only4/danmuku/adapter/application/queries/user/GetAccountInfoByEmailQryHandler.kt
@@ -4,7 +4,6 @@ import com.only4.cap4k.ddd.core.application.query.Query
 import edu.only4.danmuku.application.queries._share.model.dto.User.UserAccountInfo
 import edu.only4.danmuku.application.queries._share.model.email
 import edu.only4.danmuku.application.queries.user.GetAccountInfoByEmailQry
-import edu.only4.danmuku.domain.aggregates.user.enums.UserType
 import org.babyfish.jimmer.sql.kt.KSqlClient
 import org.babyfish.jimmer.sql.kt.ast.expression.eq
 import org.springframework.stereotype.Service
@@ -22,7 +21,6 @@ class GetAccountInfoByEmailQryHandler(
 ) : Query<GetAccountInfoByEmailQry.Request, GetAccountInfoByEmailQry.Response> {
 
     override fun exec(request: GetAccountInfoByEmailQry.Request): GetAccountInfoByEmailQry.Response {
-        // 使用 Jimmer DTO 查询用户账户信息
         val userAccountInfo = sqlClient.findOneOrNull(UserAccountInfo::class) {
             where(table.email eq request.email)
         } ?: throw IllegalArgumentException("用户不存在: ${request.email}")
