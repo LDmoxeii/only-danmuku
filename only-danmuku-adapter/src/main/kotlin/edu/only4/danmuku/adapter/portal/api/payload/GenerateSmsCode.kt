501358a0249425c501c5590e570a63d46149af37
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-20 15:19:55 +0800

    feat(account): 调整短信验证码生成逻辑
    - 修改 account_security_gen.json 中 GenerateSmsCode 的 tag 为 payload
    - 更新 GenerateSmsCode 描述信息，去除场景相关说明
    - 删除 ValidateSmsCodeCliHandler 文件及相关客户端定义
    - 新增 GenerateSmsCode API payload 定义文件
    - 调整相关类结构以适配新的调用方式

---
+++ only-danmuku-adapter/src/main/kotlin/edu/only4/danmuku/adapter/portal/api/payload/GenerateSmsCode.kt
@@ -0,0 +1,26 @@
+package edu.only4.danmuku.adapter.portal.api.payload
+
+import org.mapstruct.Mapper
+import org.mapstruct.factory.Mappers
+
+/**
+ * 生成并下发短信验证码
+ *
+ * 该文件由 [cap4k-ddd-codegen-gradle-plugin] 生成
+ * @author cap4k-ddd-codegen
+ * @date 2025/11/20
+ */
+object GenerateSmsCode {
+
+    class Request
+
+    class Response
+
+    @Mapper(componentModel = "default")
+    interface Converter {
+        companion object {
+            val INSTANCE: Converter = Mappers.getMapper(Converter::class.java)
+        }
+    }
+}
+
